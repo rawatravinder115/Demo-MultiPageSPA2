d32d99b033b02e8f93548e661ac84464640411d6
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-15 13:40:03 +0530

    add api.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from "react-router-dom";
 
-import AllQuotes from './pages/AllQuotes';
-import QuoteDetail from './pages/QuoteDetail';
-import NewQuote from './pages/NewQuote';
-import Layout from './components/layout/Layout';
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+import Layout from "./components/layout/Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Layout>
       <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
         </Route>
-        <Route path='/quotes' exact>
+        <Route path="/quotes" exact>
           <AllQuotes />
         </Route>
-        <Route path='/quotes/:quoteId'>
+        <Route path="/quotes/:quoteId">
           <QuoteDetail />
         </Route>
-        <Route path='/new-quote'>
+        <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*"> 
+        {/* /* this * means any router can match it. * */}
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,48 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import CommentsList from './CommentsList';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+
+  const {quoteId} = params;
+  const{sendRequest , status , data: loadedComments }= useHttp(getAllComments);
+
+  useEffect(()=>{
+    sendRequest(quoteId);
+  },[quoteId])
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+
+  const addedCommentHandler =() =>{
+
+  };
+
+  let comments;
+
+  if(status === 'pending'){
+    comments = <div className='centered'>
+      <LoadingSpinner/>
+    </div>;
+  }
+
+  if(status === 'completed' && loadedComments){
+    comments=<CommentsList comments ={loadedComments} />
+  }
+
+  if(status === 'completed' && (loadedComments || loadedComments.length === 0)){
+    comments = <p className = 'centered'>  No Comments were added yet!</p>
+  }
   
   return (
     <section className={classes.comments}>
@@ -18,8 +52,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId ={quoteId} onAddedComment={addedCommentHandler} />}
+      {comments}
     </section>
   );
 };
